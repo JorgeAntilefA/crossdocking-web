3a14c0fd7d940da2b49777d7f4b2b2ab4c40c40a
Jorge Eduardo  Antilef <jorge.antilef@dafitis.cl>
2020-08-06 12:02:30 -0400

    web para crossdocking

---
+++ src/components/productivity/Manifests.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "../styles/Manifests.css";
+
+class Manifests extends Component {
+  render() {
+    return <div>Hola</div>;
+  }
+}
+
+export default Manifests;
